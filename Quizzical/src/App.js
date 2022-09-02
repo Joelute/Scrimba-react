69276dc1755e809e935167e16aa9fdf8e7317321
Joseph Ho <Josephho678@gmail.com>
2022-09-01 20:40:39 -0400

    Created speed typing game

--- Quizzical/src/App.js
+++ Quizzical/src/App.js
@@ -68,7 +68,7 @@ function App() {
   }
 
   useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5')
+    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
     .then(res => res.json())
     .then(data => data.results.map(result => {
       const answerArray = []
